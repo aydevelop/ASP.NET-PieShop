46175ba0109304d14d49964f03b1144066ce15b5
aydevelop <ay.develop1@gmail.com>
2020-07-15 00:40:51 +0300

    Entity Framework init

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PieShop.Models;
@@ -13,10 +16,20 @@ namespace PieShop
 {
     public class Startup
     {
+        public IConfiguration Conf { get; }
+        public Startup(IConfiguration conf)
+        {
+            Conf = conf;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Conf.GetConnectionString("DefaultConnection"))
+            );
+
             services.AddScoped<IPieRepository, MockPieRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();
