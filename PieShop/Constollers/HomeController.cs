64175cf08ee26daf720ef8503d04faab4b88d9f5
aydevelop <ay.develop1@gmail.com>
2020-07-15 16:57:26 +0300

    Shopping cart

--- PieShop/Constollers/HomeController.cs
+++ PieShop/Constollers/HomeController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
 
 namespace PieShop.Constollers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public HomeController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
         public IActionResult Index()
         {
-            return Content("test");
+            var homeViewModel = new HomeViewModel()
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
         }
     }
 }
--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -32,6 +32,9 @@ namespace PieShop
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -45,7 +48,7 @@ namespace PieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
